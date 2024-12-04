0e063ee221ca63da7c7ea6fbdd2fae1e58a5fc1c
mohamedazizbraham <mohamed_aziz.braham@etud.univ-jfc.fr>
2024-12-04 11:28:41 +0100

    commit class Zoom (Zoom IN)

---
+++ src/main/java/tp/ZoomEffect.java
@@ -0,0 +1,28 @@
+package tp;
+
+import bandeau.Bandeau;
+
+public class ZoomEffect extends Effect {
+    private int numberOfZoomSteps;
+    private int fontSizeIncrement;
+
+    public ZoomEffect(int numberOfZoomSteps, int fontSizeIncrement) {
+        this.numberOfZoomSteps = numberOfZoomSteps;
+        this.fontSizeIncrement = fontSizeIncrement;
+    }
+
+    @Override
+    public void doEffect(Bandeau bandeau) {
+        int originalFontSize = bandeau.getFont().getSize(); // Taille de la police originale
+
+        for (int i = 0; i < numberOfZoomSteps; i++) {
+            int newFontSize = originalFontSize + (i * fontSizeIncrement); // Calcul de la nouvelle taille de la police
+            bandeau.setFont(bandeau.getFont().deriveFont((float) newFontSize)); // Appliquer la nouvelle taille
+            bandeau.sleep(100); // Pause pour visualiser l'effet
+        }
+
+
+        bandeau.setFont(bandeau.getFont().deriveFont((float) originalFontSize)); // Réinitialiser la taille de la police
+    }
+}
+

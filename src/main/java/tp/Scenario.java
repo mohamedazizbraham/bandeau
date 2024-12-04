d0a1a929f2e55f5c554a161b84b85ef3e26ad632
mohamedazizbraham <mohamed_aziz.braham@etud.univ-jfc.fr>
2024-12-04 11:13:55 +0100

    commit class Scenario

---
+++ src/main/java/tp/Scenario.java
@@ -0,0 +1,27 @@
+package tp;
+
+import bandeau.Bandeau;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Scenario {
+
+    private Bandeau bandeau;
+    private List<Effect> effects = new ArrayList<>();
+
+    public Scenario(Bandeau bandeau) {
+        this.bandeau = bandeau;
+    }
+
+    public void addEffect(Effect e) {
+        effects.add(e);
+    }
+
+    public void play() {
+        for (Effect e : effects) {
+            e.doEffect(bandeau);
+            bandeau.sleep(500);
+        }
+    }
+}

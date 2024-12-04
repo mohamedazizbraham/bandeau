8c372200b6ea62ba2ad45f5095fd2dd85a3d2eef
mohamedazizbraham <mohamed_aziz.braham@etud.univ-jfc.fr>
2024-12-04 11:43:40 +0100

    changement de Main

--- src/main/java/tp/Main.java
+++ src/main/java/tp/Main.java
@@ -6,13 +6,13 @@ public class Main {
     public static void main(String[] args) {
         Bandeau b = new Bandeau();
         ZoomEffect e1 = new ZoomEffect(20, 5);
-      //  AnimationEffect e2 = new AnimationEffect(100);
-     //   MultichiffreEffect e3 = new MultichiffreEffect(100, new Character[] {'%', '$', '£', '*', '*', 'µ', '¤', ')', '}', '{', 'ç', '#'});
+        AnimationEffect e2 = new AnimationEffect(100);
+        MultichiffreEffect e3 = new MultichiffreEffect(100, new Character[] {'%', '$', '£', '*', '*', 'µ', '¤', ')', '}', '{', 'ç', '#'});
 
         Scenario scenario = new Scenario(b);
         scenario.addEffect(e1);
-     //   scenario.addEffect(e2);
-      //  scenario.addEffect(e3);
+        scenario.addEffect(e2);
+        scenario.addEffect(e3);
         scenario.play();
     }
 }
